b97b9c80f5c15cd5f0253fce968cf52ff8d8ef76
arauj0duy <m.duda.araujo2012@gmail.com>
2026-01-06 19:40:23 -0300

    Testando commit home

--- src/HalloWelt.java
+++ src/HalloWelt.java
@@ -3,7 +3,7 @@ import br.com.sankhya.extensions.actionbutton.ContextoAcao;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class HalloWelt implements AcaoRotinaJava {// Github ->Eclipse @guilhermeNetogit 06/01/2026 15:41; Duda passou aqui 05/01/26 20:12
+public class HalloWelt implements AcaoRotinaJava {// Github <- Eclipse @guilhermeNetogit 06/01/2026 19:41; Duda passou aqui 05/01/26 20:12
 
     @Override
     public void doAction(ContextoAcao ctx) throws Exception {
@@ -18,7 +18,7 @@ public class HalloWelt implements AcaoRotinaJava {// Github ->Eclipse @guilherme
 
         // Exibe mensagem para o usuário via popup no Sankhya
         String mensagem = String.format(
-            "<b>Testando Github -> Eclipse!!</b>\n\n <b>Hallo Welt!</b>\nIhr Java-Code hat funktioniert.\nWie geht’s %s?\nHeute ist %s.",
+            "<b>Testando Github <- Eclipse!!</b>\n\n <b>Hallo Welt!</b>\nIhr Java-Code hat funktioniert.\nWie geht’s %s?\nHeute ist %s.",
             name, dataFormatada
         );
 
