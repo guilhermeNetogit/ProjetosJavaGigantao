4b49bbcdee64dad9b992d73ca36e44e8ff7858e9
arauj0duy <m.duda.araujo2012@gmail.com>
2026-01-11 15:31:04 -0300

    Adcionada a quebra de linha

--- src/br/com/sankhya/action/eventoprogramavel/BlkNewVei.java
+++ src/br/com/sankhya/action/eventoprogramavel/BlkNewVei.java
@@ -1,4 +1,5 @@
 package br.com.sankhya.action.eventoprogramavel;
+
 import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
 import br.com.sankhya.jape.event.PersistenceEvent;
 import br.com.sankhya.jape.event.TransactionContext;
@@ -6,7 +7,7 @@ import br.com.sankhya.jape.vo.DynamicVO;
 import br.com.sankhya.modelcore.MGEModelException;
 import java.math.BigDecimal;
 
-public class BlkNewVei implements EventoProgramavelJava {// Eclipse -> Github @guilhermeNetogit passou aqui em 11/01/2026 14:40:20
+public class BlkNewVei implements EventoProgramavelJava {// Eclipse -> Github @guilhermeNetogit passou aqui em 11/01/2026 15:26:45
 	
    private static final BigDecimal ANO_MINIMO = new BigDecimal("2020");
 	
@@ -48,9 +49,11 @@ public class BlkNewVei implements EventoProgramavelJava {// Eclipse -> Github @g
 		// Só avisa se o ano for anterior a 2020
 		if (anoFabric != null && anoFabric.compareTo(ANO_MINIMO) < 0) {
         // Adiciona uma mensagem de aviso (aparece em amarelo no topo da tela)
-			throw new MGEModelException(
-            "Atenção: O veículo possui ano de fabricação anterior a 2020. "
-            + "Verifique se isso está correto antes de prosseguir.\n\n Teste via Evento Modulo Java/Github 4.");
+			throw new Exception(//MGEModelException(
+            "Atenção:<br><br>"
+			+ "O veículo possui ano de fabricação anterior a 2020.<br>"
+            + "Verifique se isso está correto antes de prosseguir.<br><br>"
+            + "Teste via Evento Modulo Java/Github 5.");
         // Não lança exceção → permite salvar
     	}
 	}
